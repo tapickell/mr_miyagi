89d619bf19cb0e9c79a65309193c4d0b79ba63fa
Todd Pickell <myappleguy@gmail.com>
2013-08-10 07:14:32 -0500

    added form partial and and new action and page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,8 @@ class PostsController < ActionController::Base
   def index
     @posts = [Post.new("Mr Miyagi", Time.now, "Mr Miyagi Teaches Ruby", "Today you will learn rails like a real developer, without scaffolding.")]
   end
+
+  def new
+    @post = Post.new
+  end
 end
