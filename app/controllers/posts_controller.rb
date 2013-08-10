dddabad79b832c43180690499f5f4508c9e88399
Todd Pickell <myappleguy@gmail.com>
2013-08-09 23:14:45 -0500

    just worked with view and added link for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ActionController::Base
   def index
-    @posts = [Post.new("Mr Miyagi", Time.now, "Mr Miyagi Teaches Ruby", "Today you will learn rails like a real developer, without generators.")]
+    @posts = [Post.new("Mr Miyagi", Time.now, "Mr Miyagi Teaches Ruby", "Today you will learn rails like a real developer, without scaffolding.")]
   end
 end
