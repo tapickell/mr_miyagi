88a0cb52f6639aab2856ae354fd9677b78aac94e
Todd Pickell <myappleguy@gmail.com>
2013-08-10 08:35:32 -0500

    tried changing to actvie model to support form_for

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ActionController::Base
   def index
-    @posts = [Post.new("Mr Miyagi", Time.now, "Mr Miyagi Teaches Ruby", "Today you will learn rails like a real developer, without scaffolding.")]
+    @posts = [Post.new("Mr Miyagi", "Mr Miyagi Teaches Ruby", "Today you will learn rails like a real developer, without scaffolding.")]
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
-class Post < Struct.new(:author, :published_on, :title, :body)
-
+class Post < ActiveRecord::Base
+  attr_reader :title, :author, :body, :published_on
+  def initialize(author="", title="", body="")
+    @author = author
+    @title = title
+    @body = body
+    @published_on = Time.now
+  end
 end
